69abb317fa12ce7ddb0ba0b574d9cf0e38b2abfe
Andres Cardona <andres.cardo98@gmail.com>
2023-03-22 00:14:40 -0500

    Tournament getters and setters

--- src/com/andres/fpc/domain/Player.java
+++ src/com/andres/fpc/domain/Player.java
@@ -1,5 +1,42 @@
 package com.andres.fpc.domain;
 
 public class Player {
-    public String name;
+    private String name;
+    private String position;
+    private String skillfulLeg;
+    private int dorsal;
+
+    public Player(String name, String position, String skillfulLeg) {
+        this.name = name;
+        this.position = position;
+        this.skillfulLeg = skillfulLeg;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getSkillfulLeg() {
+        return skillfulLeg;
+    }
+
+    public int getDorsal() {
+        return dorsal;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public void setDorsal(int dorsal) {
+        this.dorsal = dorsal;
+    }
 }
--- src/com/andres/fpc/domain/Tournament.java
+++ src/com/andres/fpc/domain/Tournament.java
@@ -3,13 +3,44 @@ package com.andres.fpc.domain;
 import java.util.List;
 
 public class Tournament {
-    public List<Team> teams;
+    private List<Team> teams;
+    private String name;
+    private String type;
+    private int semester;
+
 
     public void printTeamPlayers(){
         for (Team t : teams){
-            for (Player p : t.players){
-                System.out.println(p.name);
+            for (Player p : t.getPlayers()){
+                System.out.println(p.getName());
             }
         }
+    }
+    public List<Team> getTeams(){
+        return teams;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+
+    public void setTeams(List<Team> teams) {
+        this.teams = teams;
     }
 }
