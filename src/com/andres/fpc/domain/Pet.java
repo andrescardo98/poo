8fa01d07080cf87db0ce5290c8e1a8b71f933ea1
Andres Cardona <andres.cardo98@gmail.com>
2023-03-22 00:15:27 -0500

    Tournament getters and setters

--- src/com/andres/fpc/domain/Pet.java
+++ src/com/andres/fpc/domain/Pet.java
@@ -1,4 +1,13 @@
 package com.andres.fpc.domain;
 
 public class Pet {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
