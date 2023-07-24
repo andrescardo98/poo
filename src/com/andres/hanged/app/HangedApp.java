2229d5942482dd968121edc0f2e3b100a9654b99
Andres Cardona <andres.cardo98@gmail.com>
2023-07-23 22:58:32 -0500

    Hanged game

---
+++ src/com/andres/hanged/app/HangedApp.java
@@ -0,0 +1,11 @@
+package com.andres.hanged.app;
+
+import com.andres.hanged.domain.Hanged;
+
+public class HangedApp {
+    public static void main(String[] args) {
+        Hanged hanged = new Hanged();
+        hanged.play();
+
+    }
+}
